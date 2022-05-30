bb723012c2a53fa90f5be2f57862eb789802f5e5
ihsanhamid <sanzgemink91@gmail.com>
2022-05-30 16:39:58 +0800

    first commit

---
+++ assets/js/ques.js
@@ -0,0 +1,62 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Agar tubuhku sehat dan kuat maka aku",
+    answer: "Makan makanan 4 sehat 5 sempurna",
+    options: [
+      "Menonton tv setiap hari",
+      "Membaca buku setiap waktu",
+      "Bermain bersama teman",
+      "Makan makanan 4 sehat 5 sempurna"
+    ]
+  },
+
+    {
+    numb: 2,
+    question: "Bermain membuat aku",
+    answer: "Gembira",
+    options: [
+      "Gembira",
+      "Sedih",
+      "Kesal",
+      "Marah"
+    ]
+  },
+
+  {
+    numb: 3,
+    question: "Kegiatan sehari-hari harus dilakukan dengan",
+    answer: "Ceria",
+    options: [
+      "Kesal",
+      "Ceria",
+      "Sedih",
+      "Marah"
+    ]
+  },
+
+  {
+    numb: 4,
+    question: "1+5+3 = ?",
+    answer: "9",
+    options: [
+      "8",
+      "Tujuh",
+      "09",
+      "9"
+    ]
+  },
+
+  {
+    numb: 5,
+    question: "kapan hari kemerdekaan indonesia ?",
+    answer: "17 Agustus",
+    options: [
+      "15 Agustus",
+      "17 Januari",
+      "17 Agustus",
+      "8 July"
+    ]
+  }
+];
